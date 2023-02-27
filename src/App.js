d7390b0e3ebd6856b8f6e8f4971209d864e2cd03
Mehrnaz Khorrami <mehrnazkhorrami@gmail.com>
2023-02-27 18:12:22 +0330

    makes displaye items like list and their delete button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+   
+  const [input, setInput] = useState('');
+  const [list, setList] = useState([]);
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+
+  function addItem(e) {
+    e.preventDefault();
+    if(!input){
+      alert('Enter your item, please!')
+    } else {
+      setList((addedItem)=> [...addedItem, input]);
+      setInput('');
+    } 
+  }
+
+  function handleDelete(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
      <div className='input'>
-      <input type="text" placeholder='Add your items...' />
-      <button>Add</button>
+      <input type="text" placeholder='Add your items...' value={input} onChange={handleChange}/>
+      <button className='add' onClick={addItem}>Add</button>
      </div>
+     
+      {
+        list.map((items) => (
+          <div className='displayItem'>
+            <li>{items}
+              <button className='delete' onClick={handleDelete}>Delete</button>
+            </li>
+          </div>
+        ))
+      }
+     
     </div>
   );
 }
